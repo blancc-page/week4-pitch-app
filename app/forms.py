ac24925be8ee8a863d192a9eda724679675afe47
blancc-page <blancc.666@gmail.com>
2022-05-11 01:22:49 +0300

    first commit

--- app/forms.py
+++ app/forms.py
@@ -3,7 +3,6 @@ from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import DataRequired
 
 class ReviewForm(FlaskForm):
-
     title = StringField('Review title',validators=[DataRequired()])
     review = TextAreaField('Movie review', validators=[DataRequired()])
     submit = SubmitField('Submit') 
