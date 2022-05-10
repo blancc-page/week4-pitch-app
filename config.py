a2d1fa7d5f970e6eb6e783cd42181b18ba86c088
blancc-page <blancc.666@gmail.com>
2022-05-11 02:03:37 +0300

    first commit

--- config.py
+++ config.py
@@ -1,10 +1,8 @@
 import os
-  
-  
-  
+
   
 class Config:
-    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:dclxvi@localhost:4472/pitch'
+    SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:dclxvi@127.0.0.0:5432/pitch'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     SECRET_KEY=os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
