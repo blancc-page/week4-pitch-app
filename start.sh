cc5c74bd00b182bfa0e4b68c3a592ffdd2a85302
Moses Muta <65918325+blancc-page@users.noreply.github.com>
2022-07-21 00:16:57 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-export MAIL_USERNAME='motest69@gmail.com'
-export MAIL_PASSWORD='killa911G?'
-export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://postgres:dclxvi@localhost/pitch'
-export SQLALCHEMY_TRACK_MODIFICATIONS=True
-export SECRET_KEY="purplesector"
-
-
-py manage.py runserver
